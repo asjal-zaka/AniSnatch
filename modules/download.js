06eaef37951c11cd53858fed96cb1da0c09abc46
Asjal Zaka <77961653+asjal16@users.noreply.github.com>
2024-06-22 20:47:39 +0500

    Update download.js

--- modules/download.js
+++ modules/download.js
@@ -1,49 +1,21 @@
-import puppeteer from 'puppeteer-core';
-import chromium from 'chrome-aws-lambda';
-import axios from "axios";
-import { load } from "cheerio";
-
-import { getEpisodeList } from "./episodes.js";
-
-const baseURL = "https://anitaku.to";
-
-const id = "one-piece-dub"
-
-export const getLinks = async (epID) => {
-    const searchURL = await axios.get(`${baseURL}/${epID}`);
-    const $ = load(await searchURL.data);
-    const link = $('.dowloads').find('a').attr('href');
-    return link;
-}
-
+import { chromium } from 'playwright';
 
 export const getDownload = async (link) => {
   let browser = null;
   try {
-    browser = await puppeteer.launch({
-      args: [
-        ...chrome.args,
-        '--disable-web-security',
-        '--ignore-certificate-errors',
-        '--no-sandbox',
-        '--disable-setuid-sandbox',
-        '--single-process'
-      ],
-      defaultViewport: chrome.defaultViewport,
-      executablePath: await chrome.executablePath,
-      headless: chrome.headless,
+    browser = await chromium.launch({
+      headless: true, // Adjust as needed
     });
 
     const page = await browser.newPage();
-    await page.goto(link, { waitUntil: 'networkidle2' });
-    await page.waitForSelector('.dowload'); 
-    const links = await page.$$eval('.dowload a', anchors => {
-      return anchors.map(anchor => anchor.href);
-    });
+    await page.goto(link, { waitUntil: 'networkidle' });
+
+    await page.waitForSelector('.dowload');
+    const links = await page.$$eval('.dowload a', anchors => anchors.map(anchor => anchor.href));
 
     return links;
   } catch (error) {
-    console.error('Error in Puppeteer:', error);
+    console.error('Error in Playwright:', error);
     throw error;
   } finally {
     if (browser !== null) {
