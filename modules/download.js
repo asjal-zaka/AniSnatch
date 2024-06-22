fc89460f120378886fdb19431f1e965eaf83a7ff
asjal16 <asjalzaka16@gmail.com>
2024-06-22 17:07:32 +0500

    Updating donwload.js

--- modules/download.js
+++ modules/download.js
@@ -1,5 +1,5 @@
-
-import puppeteer from "puppeteer"
+import puppeteer from 'puppeteer-core';
+import chrome from 'chrome-aws-lambda';
 import axios from "axios";
 import { load } from "cheerio";
 
@@ -16,15 +16,38 @@ export const getLinks = async (epID) => {
     return link;
 }
 
+
 export const getDownload = async (link) => {
-    const browser = await puppeteer.launch( {headless: true} );
+  let browser = null;
+  try {
+    browser = await puppeteer.launch({
+      args: [
+        ...chrome.args,
+        '--disable-web-security',
+        '--ignore-certificate-errors',
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--single-process'
+      ],
+      defaultViewport: chrome.defaultViewport,
+      executablePath: await chrome.executablePath,
+      headless: chrome.headless,
+    });
+
     const page = await browser.newPage();
     await page.goto(link, { waitUntil: 'networkidle2' });
-    await page.waitForSelector('.dowload');
+    await page.waitForSelector('.dowload'); 
     const links = await page.$$eval('.dowload a', anchors => {
       return anchors.map(anchor => anchor.href);
     });
-    await browser.close();
 
     return links;
-  };
+  } catch (error) {
+    console.error('Error in Puppeteer:', error);
+    throw error;
+  } finally {
+    if (browser !== null) {
+      await browser.close();
+    }
+  }
+};
